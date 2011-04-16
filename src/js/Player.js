a256f1a9f5c93d32c8a32a05c1ece752564400cd
Philip Guerrant <pguerrant@gmail.com>
2011-04-16 13:56:07 -0600

    implemented lazy loading and saving id3 info to store

--- src/js/Player.js
+++ src/js/Player.js
@@ -26,14 +26,14 @@ Ext.define('Xap.Player', {
     volume: 75,
 
     /**
-     * @cfg {Boolean} lazyLoad
+     * @cfg {Boolean} lazy
      * If true, player will load just enough of each file in playlist to determine id3 info and track length and wait until track
      * is played to load the entire file. If false, player will load all files in playlist up front.
      * Lazy loading may result in a perceived performance improvement, although total download size will be greater, since player has to load
      * a portion of each file, then stop the download once meta info is retrieved, then when the file is played, redownload the entire file.
      * Defaults to true
      */
-    lazyLoad: true,
+    lazy: true,
 
     /**
      * @cfg {Array} files
@@ -169,20 +169,12 @@ Ext.define('Xap.Player', {
      */
     load: function(files) {
         var me = this,
-            bind = Ext.Function.bind,
             currentTrackIndex = me.currentTrackIndex;
 
         Ext.Array.forEach(files, function(url) {
             // create a SoundManager Sound object and add it to the store
-            var smSound = soundManager.createSound({
-                id: url,
-                url: url,
-                onload: bind(me.updateTrackLengthFinal, me),
-                whileloading: bind(me.updateTrackLengthEstimate, me),
-                whileplaying: bind(me.updateTrackPosition, me),
-                onid3: bind(me.updateTrackInfo, me),
-                onfinish: bind(me.onTrackFinish, me)
-            });
+            var smSound = me.createSmSound(url, true);
+            smSound.load();
             me.store.add({url: url, smSound: smSound});
         });
         // Set the current track to the first track in the store, unless, of course we already have a current track.
@@ -217,7 +209,7 @@ Ext.define('Xap.Player', {
             });
         }
         // if we got here "files" is a string referring to a single file url
-        index = store.find('url', files, 0, false, true, true);
+        index = me.getTrackIndexById(files);
         if(index === me.currentTrackIndex) {
             // if we're unloading the current track lets move to the next track before we destroy the current one
             me.moveNext();
@@ -364,7 +356,7 @@ Ext.define('Xap.Player', {
         smSound = me.getCurrentSmSound();  // get the smSound at the new index
         smSound.load();
         me.updateTrackPosition(0);
-        me.updateTrackInfo();
+        me.updateTrackInfoDisplay();
         if(smSound.loaded) {
             me.updateTrackLengthFinal();
         } else if(smSound.isBuffering) {
@@ -392,12 +384,52 @@ Ext.define('Xap.Player', {
     },
 
     // private
-    updateTrackInfo: function() {
-        var info = this.getCurrentSmSound().id3;
-        this.trackInfo.set({
+    createSmSound: function(url) {
+        var me = this,
+            bind = function(fn) {
+                // sm2 event handlers are scoped to the sm2 sound object by default and have no args.
+                // this changes the scope to the current "Player" instance and passes the sm2 sound as the 1st arg
+                return function() {
+                    fn.call(me, this);
+                }
+            };
+
+        return soundManager.createSound({
+            id: url,
+            url: url,
+            onload: bind(me.onLoad),
+            onid3: bind(me.onId3),
+            whileloading: bind(me.onWhileLoading),
+            whileplaying: bind(me.onWhilePlaying),
+            onfinish: bind(me.onTrackFinish)
+        });
+    },
+
+    // private
+    updateTrackInfoDisplay: function() {
+        this.trackInfo.set(this.getId3Info(this.getCurrentSmSound()));
+    },
+
+    // private
+    getId3Info: function(smSound) {
+        var info = smSound.id3;
+        return {
             artist: info.TPE1,
             title: info.TIT2
-        });
+        };
+    },
+
+    // private
+    onId3: function(smSound) {
+        // save the id3 info to the record in the store
+        this.getTrackById(smSound.sID).set(this.getId3Info(smSound));
+        if(this.isCurrentSmSound(smSound)) {
+            this.updateTrackInfoDisplay();
+        } else if(this.lazy) {
+            // if running in lazy loading mode, unlaod the track once we have the id3 info.
+            // the track will reload when it is played
+            smSound.unload();
+        }
     },
 
     // private
@@ -408,6 +440,20 @@ Ext.define('Xap.Player', {
     // private
     updateTrackLengthFinal: function() {
         this.updateTrackLength(this.getCurrentSmSound().duration);
+    },
+
+    // private
+    onWhileLoading: function(smSound) {
+        if(this.isCurrentSmSound(smSound)) {
+            this.updateTrackLengthEstimate();
+        }
+    },
+
+    // private
+    onLoad: function(smSound) {
+        if(this.isCurrentSmSound(smSound)) {
+            this.updateTrackLengthFinal();
+        }
     },
 
     // private
@@ -425,6 +471,11 @@ Ext.define('Xap.Player', {
             this.trackInfo.set({timeElapsed: position});
             this.trackSlider.setValue(position);
         }
+    },
+
+    // private
+    onWhilePlaying: function(smSound) {
+        this.updateTrackPosition();
     },
 
     // private
@@ -460,9 +511,24 @@ Ext.define('Xap.Player', {
     },
 
     // private
-    getCurrentSmSound: function(){
+    getCurrentSmSound: function() {
         var currentTrack = this.store.getAt(this.currentTrackIndex);
         return currentTrack ? currentTrack.get('smSound') : null;
+    },
+
+    // private
+    isCurrentSmSound: function(smSound) {
+        return smSound === this.getCurrentSmSound();
+    },
+
+    // private
+    getTrackById: function(id) {
+        return this.store.findRecord('url', id, 0, false, true, true);
+    },
+
+    // private
+    getTrackIndexById: function(id) {
+        return this.store.find('url', id, 0, false, true, true);
     }
 
 });
