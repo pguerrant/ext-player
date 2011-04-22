6f5d947db964f6d9158e3575d6b3f3df87d4d9d2
Philip Guerrant <pguerrant@gmail.com>
2011-04-21 20:59:09 -0600

    removed js libs from build

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 # combine source files into player-debug.js
 cat \
-lib/ext/ext-all-debug.js \
-lib/sm2/soundmanager2.js \
 src/js/Format.js \
 src/js/Format.js \
 src/js/Templates.js \
@@ -16,5 +14,5 @@ src/js/Player.js \
 # compress player-debug.js to player.js
 java -jar /opt/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar build/player-debug.js -o build/player.js
 
-# compress xap.css
+# compress player.css
 java -jar /opt/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar src/css/player.css -o build/player.css
